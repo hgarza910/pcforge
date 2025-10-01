e345bbe145bb2da840e49ea06ddda23a2fddfd1f
Junior <youremail@example.com>
2025-09-30 17:57:16 -0700

    updated forge

--- public/forge/flutter_service_worker.js
+++ public/forge/flutter_service_worker.js
@@ -25,10 +25,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "a3e8febfd2440b20d52f745125449a58
 "canvaskit/skwasm_heavy.js.symbols": "3c01ec03b5de6d62c34e17014d1decd3",
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "5e08059ec01c3aae0f643d24efd3739f",
+"flutter_bootstrap.js": "8b995ce9fd9cb77db7a2acf33556f3be",
 "index.html": "f46b0a971de7ecd33ee76b6c0640c4e5",
 "/": "f46b0a971de7ecd33ee76b6c0640c4e5",
-"main.dart.js": "1f662fded48d5ca04bd20549143dc66f",
+"main.dart.js": "f1c34eaca0892343b2327dbe05a405dc",
 "styles.css": "4e5e8853325af6d45456a3de905da6b6",
 "version.json": "aaa52b13abc93d87ae7139a3d9dadb50"};
 // The application shell files that are downloaded before a service worker can
