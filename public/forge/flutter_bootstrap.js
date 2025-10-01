538e512d06e662cd58e8626dffcfcf1137e27839
Junior <youremail@example.com>
2025-10-01 16:07:20 -0700

    updated forge

--- public/forge/flutter_bootstrap.js
+++ public/forge/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "735737951"
+    serviceWorkerVersion: "2567019115"
   }
 });
--- public/forge/flutter_service_worker.js
+++ public/forge/flutter_service_worker.js
@@ -25,10 +25,10 @@ const RESOURCES = {"assets/AssetManifest.bin": "a3e8febfd2440b20d52f745125449a58
 "canvaskit/skwasm_heavy.js.symbols": "3c01ec03b5de6d62c34e17014d1decd3",
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "6ede72e2ad8c06f89784c9b7bb9787db",
+"flutter_bootstrap.js": "d7d8bd2816c1f4b1b0ff1ca8c24af005",
 "index.html": "f46b0a971de7ecd33ee76b6c0640c4e5",
 "/": "f46b0a971de7ecd33ee76b6c0640c4e5",
-"main.dart.js": "f5bcb9950581ec6c70d5103a5b0d384c",
+"main.dart.js": "46f9ea495b5b631eda7130ef9d7d81ff",
 "styles.css": "4e5e8853325af6d45456a3de905da6b6",
 "version.json": "aaa52b13abc93d87ae7139a3d9dadb50"};
 // The application shell files that are downloaded before a service worker can
